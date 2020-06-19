2e2a466dc0ecb370b9abe65f702333997f09fc5f
thinkerwolf <1342125135@qq.com>
2020-06-19 16:27:08 +0800

    GamerRegistry

--- gamer-spring-boot-starter/src/main/java/com/thinkerwolf/gamer/GamerRegistryConfiguration.java
+++ gamer-spring-boot-starter/src/main/java/com/thinkerwolf/gamer/GamerRegistryConfiguration.java
@@ -5,30 +5,25 @@ import com.thinkerwolf.gamer.common.URL;
 import com.thinkerwolf.gamer.properties.GamerRegistryProperties;
 import com.thinkerwolf.gamer.registry.Registry;
 import com.thinkerwolf.gamer.registry.RegistryFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties({GamerRegistryProperties.class})
-@ConditionalOnProperty(prefix = "gamer.registry", name = "enabled", matchIfMissing = true)
-public class GamerRegistryConfiguration implements ApplicationContextAware {
+public class GamerRegistryConfiguration {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
+    @Configuration
+    @ConditionalOnProperty(prefix = "gamer.registry", name = "enabled", matchIfMissing = true)
+    public static class RegistryConfiguration {
+        @Bean
+        public Registry registry(GamerRegistryProperties registryProperties) throws Exception {
+            URL url = URL.parse(registryProperties.getClient().getServiceUrl());
+            RegistryFactory factory = ServiceLoader.getService(url.getProtocol(), RegistryFactory.class);
+            return factory.create(url);
+        }
     }
-
-    @Bean
-    public Registry registry(GamerRegistryProperties registryProperties) throws Exception {
-        URL url = URL.parse(registryProperties.getClient().getServiceUrl());
-        RegistryFactory factory = ServiceLoader.getService(url.getProtocol(), RegistryFactory.class);
-        return factory.create(url);
-    }
-
 
 }
