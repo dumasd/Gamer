92bd294b2ad323778f1035828fe628913782ce9d
thinkerwolf <1342125135@qq.com>
2020-08-14 13:11:09 +0800

    Grizzly

--- gamer-remoting/gamer-remoting-grizzly/src/main/java/com/thinkerwolf/gamer/grizzly/FilterChains.java
+++ gamer-remoting/gamer-remoting-grizzly/src/main/java/com/thinkerwolf/gamer/grizzly/FilterChains.java
@@ -8,11 +8,16 @@ import com.thinkerwolf.gamer.grizzly.websocket.WebSocketClientFilter;
 import com.thinkerwolf.gamer.grizzly.websocket.WebSocketServerFilter;
 import com.thinkerwolf.gamer.remoting.ChannelHandler;
 import com.thinkerwolf.gamer.remoting.Protocol;
+import com.thinkerwolf.gamer.remoting.ssl.SslConfig;
+import org.apache.commons.lang.StringUtils;
 import org.glassfish.grizzly.Processor;
 import org.glassfish.grizzly.filterchain.FilterChainBuilder;
 import org.glassfish.grizzly.filterchain.TransportFilter;
 import org.glassfish.grizzly.http.HttpClientFilter;
 import org.glassfish.grizzly.http.HttpServerFilter;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
+import org.glassfish.grizzly.ssl.SSLFilter;
 import org.glassfish.grizzly.utils.DelayedExecutor;
 import org.glassfish.grizzly.utils.IdleTimeoutFilter;
 import org.glassfish.grizzly.websockets.WebSocketEngine;
@@ -35,6 +40,12 @@ public final class FilterChains {
             final DelayedExecutor timeoutExecutor = IdleTimeoutFilter.createDefaultIdleDelayedExecutor();
             timeoutExecutor.start();
             builder.addLast(new TransportFilter());
+
+            SSLEngineConfigurator cfg = initializeSSL(url);
+            if (cfg != null) {
+                builder.addLast(new SSLFilter(cfg, cfg.copy().setClientMode(true)));
+            }
+
             builder.addLast(server ? new HttpServerFilter() : new HttpClientFilter());
             if (server || protocol.equals(Protocol.WEBSOCKET)) {
                 builder.addLast(server ? new WebSocketServerFilter() : new WebSocketClientFilter());
@@ -42,6 +53,26 @@ public final class FilterChains {
             builder.addLast(server ? new GrizzlyServerFilter(url, handler) : new GrizzlyClientFilter(url, handler));
         }
         return builder.build();
+    }
+
+    /**
+     * Initialize server side SSL configuration.
+     */
+    private static SSLEngineConfigurator initializeSSL(URL url) {
+        SSLContextConfigurator sslContextConfig = new SSLContextConfigurator();
+        SslConfig cfg = url.getAttach(URL.SSL);
+        if (cfg == null || !cfg.isEnabled()) {
+            return null;
+        }
+        if (StringUtils.isNotBlank(cfg.getKeystoreFile())) {
+            sslContextConfig.setKeyStoreFile(cfg.getKeystoreFile());
+            sslContextConfig.setKeyStorePass(cfg.getKeystorePass());
+        }
+        if (StringUtils.isNotBlank(cfg.getTruststoreFile())) {
+            sslContextConfig.setTrustStoreFile(cfg.getTruststoreFile());
+            sslContextConfig.setTrustStorePass(cfg.getTruststorePass());
+        }
+        return new SSLEngineConfigurator(sslContextConfig.createSSLContext(false), false, false, false);
     }
 
 
